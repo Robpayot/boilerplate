bd2de151020ff53f76d6928d768b046873d6e6e3
Robin Payot <robpayot@gmail.com>
2019-04-03 14:09:14 -0700

    remove blank line

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,6 @@ const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-
 module.exports = merge(common, {
 	mode: 'production',
 	devtool: 'source-map',
